16e9d35b592801d2713aaeabce1a1ec5e4b6d6e1
daideguchi <dd.1107.11107@gmail.com>
2022-09-09 12:51:26 +0900

    220909

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -130,7 +130,7 @@ export default function Layout({ children, title = "myprofile site" }) {
 
       <footer class="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900">
         <div class="sm:flex sm:items-center sm:justify-between">
-          <a
+          {/* <a
             href="https://flowbite.com/"
             class="flex items-center mb-4 sm:mb-0"
           >
@@ -144,7 +144,7 @@ export default function Layout({ children, title = "myprofile site" }) {
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               NFT-Garege
             </span>
-          </a>
+          </a> */}
           <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" class="mr-4 hover:underline md:mr-6 ">
