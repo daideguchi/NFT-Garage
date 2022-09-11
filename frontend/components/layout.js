3615db8d680d354701264977da88af19887b5eed
daideguchi <dd.1107.11107@gmail.com>
2022-09-11 18:09:09 +0900

    220911

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { Dropdown } from "flowbite-react";
+// import { Dropdown } from "flowbite";
 // import { account, chainId } from "../pages/index";
 
 export default function Layout({ children, title = "myprofile site" }) {
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.js",
+    // "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{ts,tsx}",
     "./public/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx}",
